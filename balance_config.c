45d4075bdc894fbe7a48ea081ba9add10af76b0c
alevesque <levesque.alejandro@gmail.com>
2017-04-18 17:29:16 -0700

    Create balance_config.c
    Create config file for defined terms.

---
+++ balance_config.c
@@ -0,0 +1,62 @@
+/*******************************************************************************
+* balance_config.c
+*
+* Contains the settings struct for configuration of balance.c
+* Settings are contained here for neatness.
+* Macros are used to take CONFIG_TABLE and both declare a struct
+* and then fill it with the default values.
+*******************************************************************************/
+
+#ifndef BALANCE_CONFIG
+#define BALANCE_CONFIG
+
+#define SAMPLE_RATE_HZ 200	// main filter and control loop speed
+#define DT 0.005      		// 1/sample_rate
+
+// Structural properties of eduMiP
+
+#define 	CAPE_MOUNT_ANGLE		0.40//  0.35 for blUE
+#define 	GEARBOX 				35.577
+#define 	ENCODER_RES				60
+#define 	WHEEL_RADIUS_M			0.034
+#define 	TRACK_WIDTH_M			0.035
+#define 	V_NOMINAL				7.4
+
+// inner loop controller 200hz
+#define 	D1_GAIN					-3.5
+#define 	D1_ORDER				2
+#define 	D1_NUM					{1,0.14,0.0049}
+#define 	D1_DEN					{1, 0.707, 0.0049}
+#define 	D1_SATURATION_TIMEOUT	0.5
+
+// outer loop controller original 20hz
+#define		D2_GAIN					0.3
+#define		D2_ORDER				1
+#define		D2_NUM					{ 1, -1 }
+#define		D2_DEN					{ 1.0000, -0.9 }
+#define 	THETA_REF_MAX	0.37
+
+// electrical hookups
+#define 	MOTOR_CHANNEL_L 		4
+#define 	MOTOR_CHANNEL_R 		1
+#define 	MOTOR_POLARITY_L 		1
+#define 	MOTOR_POLARITY_R 		-1
+#define 	ENCODER_CHANNEL_L 		3
+#define 	ENCODER_CHANNEL_R 		2
+#define 	ENCODER_POLARITY_L 		1
+#define 	ENCODER_POLARITY_R 		-1
+
+// Thread Loop Rates
+#define		BATTERY_CHECK_HZ		5
+#define 	REFERENCE_VALUE_MANAGER_HZ	100
+#define		PRINTF_HZ			50
+#define		SAMPLE_FREQ_OUTER		20
+// other
+#define 	TIP_ANGLE 			0.75
+#define 	START_ANGLE 			0.3	
+#define 	START_DELAY 			0.5	
+#define 	PICKUP_DETECTION_TIME 	0.65
+#define 	ENABLE_POSITION_HOLD	1
+#define 	SOFT_START_SEC			0.7
+
+#endif //BALANCE_CONFIG
