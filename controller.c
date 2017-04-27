152b3c6b97ec87ed1425a3af042dd17eaa3615e8
alevesque <levesque.alejandro@gmail.com>
2017-04-27 02:39:24 -0700

    added DSM input, PWM output, general radio stuff

--- controller.c
+++ controller.c
@@ -13,19 +13,13 @@ typedef enum armstate_t{
 } armstate_t;
 
 /*******************************************************************************
-* reference_value_t
+* controller_arming_t
 *	
-* Controller reference_value written to by reference_value_manager and read by the controller.
+* Controller arming state written to by controller_arming_manager and read by the controller.
 *******************************************************************************/
-typedef struct reference_value_t{ //setpoints
+typedef struct controller_arming_t{ //setpoints
 	armstate_t armstate;	// see armstate_t declaration
-	float theta;			// body lean angle (rad)
-	float phi;				// wheel position (rad)
-	float right_pulley_angle_ref;	// pulley rotation
-	float left_pulley_angle_ref;
-	float angle_about_x_axis_ref; //body angle about x axis
-	float angle_about_y_axis_ref; //body angle about y axis
-} reference_value_t;
+} controller_arming_t;
 
 /*******************************************************************************
 * core_state_t
@@ -37,11 +31,11 @@ typedef struct core_state_t{
 	float left_pulley_angle;
 	float angle_about_x_axis; 		// body angle radians
 	float angle_about_y_axis; 		// body angle radians
-	float weightshift_dist;			//distance of weight from neutral position 
+	float weightshift_dist;			//distance of weight from neutral position -- need to know how to read, wait on physical implementation
 	float battery_voltage; 		// battery voltage 
-	float u1;			// output of controller to weight shift
-	float u2_left;			// output of controller to pulley motors
-	float u2_right;
+	float WS_duty_signal;			// output of controller to weight shift
+	float BL_duty_signal_left;			// output of controller to pulley motors
+	float BL_duty_signal_right;
 	int start_cond;
 } core_state_t;
 
@@ -62,13 +56,14 @@ typedef struct orientation_t{
 * Local Function declarations	
 *******************************************************************************/
 // IMU interrupt routine
-int control_tilt();
+int collect_data();
 
 // threads
-void* reference_value_manager(void* ptr);
+void* controller_arming_manager(void* ptr);
 void* battery_checker(void* ptr);
 void* printf_loop(void* ptr);
 void* read_input(void* ptr);
+void* check_radio_signal(void* ptr);
 
 // regular functions
 int is_flying();
@@ -82,7 +77,7 @@ int on_pause_released();
 * Global Variables				
 *******************************************************************************/
 core_state_t sys_state;
-reference_value_t reference_value;
+controller_arming_t controller_arming;
 rc_imu_data_t data;
 orientation_t orientation;
 
@@ -95,7 +90,7 @@ rc_filter_t highpass_y;
 /*******************************************************************************
 * main()
 *
-* Initialize the filters, IMU, threads, & wait until shut down
+* Initialize the filters, IMU, DSM, threads, & wait until shut down
 *******************************************************************************/
 int main(){
 	rc_set_cpu_freq(FREQ_1000MHZ);
@@ -106,13 +101,19 @@ int main(){
 	}
 	rc_set_state(UNINITIALIZED);
 
+	//initialize radio receiver
+	rc_initialize_dsm();
 
-	// make sure reference_value starts at normal values
-	reference_value.armstate = DISARMED;
+	// make sure controller_arming starts at normal values
+	controller_arming.armstate = DISARMED;
 		
+
+	/******************************************
+	* Set up filters for data gathering.
+	******************************************/	
 	//timestep, dt
 	double const dt = 1.0/(float)SAMPLE_RATE_HZ;
-	//rise time, tr **********************************************find new rise time if needed, prob need for high pass cuz noisy************************
+	//rise time, tr 
 	double const tr = 1;
 	//time constant from rise time
 	float const tau = tr/2.2;
@@ -123,10 +124,22 @@ int main(){
 	lowpass_y  = rc_first_order_lowpass(dt, tau);
 	highpass_y  = rc_first_order_highpass(dt, tau);
 
+
+
+
+
 	// set up button handlers
 	rc_set_pause_pressed_func(&on_pause_pressed);
 	rc_set_mode_released_func(&on_pause_released);
 	
+	
+	//can use gps header for gpio if needed, where WS_MOTOR_CHANNEL is the gpio pin #
+	//rc_gpio_set_dir(WS_MOTOR_CHANNEL, OUTPUT_PIN);
+	//rc_set_pinmux_mode(WS_MOTOR_CHANNEL, PINMUX_PWM);
+
+	
+
+
 	// start a thread to slowly sample battery 
 	pthread_t battery_thread;
 	pthread_create(&battery_thread, NULL, battery_checker, (void*) NULL);
@@ -153,16 +166,21 @@ int main(){
 		return -1;
 	}
 	
-	// start stack to control reference_values
-	pthread_t reference_value_thread;
-	pthread_create(&reference_value_thread, NULL, reference_value_manager, (void*) NULL);
+	// start thread to control arming/disarming controller.
+	pthread_t controller_arming_thread;
+	pthread_create(&controller_arming_thread, NULL, controller_arming_manager, (void*) NULL);
 
 	//start thread for reading user input
 	pthread_t read_input_thread;
 	pthread_create(&read_input_thread, NULL, read_input, (void*) NULL);
 	
+	//thread for ensuring transmitter signal is being received
+	pthread_t check_radio_signal_thread
+	pthread_create(&check_radio_signal_thread, NULL, check_radio_signal, (void*) NULL);
+
 	//set imu interrupt function
-	rc_set_imu_interrupt_func(&control_tilt);
+	rc_set_imu_interrupt_func(&collect_data);
+	rc_set_new_dsm_data_func(&read_input);
 		
 	rc_set_state(RUNNING);
 	rc_set_led(RED,0);
@@ -178,6 +196,7 @@ int main(){
 	rc_free_filter(&highpass_x);
 	rc_free_filter(&lowpass_y);
 	rc_free_filter(&highpass_y);
+	rc_stop_dsm_service();
 	rc_power_off_imu();
 	rc_set_cpu_freq(FREQ_ONDEMAND);
 	rc_cleanup();
@@ -185,20 +204,35 @@ int main(){
 	return 0;
 }
 
+/*******************************************************************************
+void* check_radio_signal(void* ptr)
+*
+* Detects if radio signal is being received by Beaglebone and if not, do something.
+*******************************************************************************/
+void* check_radio_signal(void* ptr){
+		
+	while(rc_get_state()!=EXITING){
+		if(!rc_is_dsm_active()){ //if not active
+			//set motors to neutral or to circle or to RTB until signal is received
+		}	
+		rc_usleep(1000000/CHECK_RADIO_SIGNAL_HZ); 
+	}
+	return NULL;
+}
 
 /*******************************************************************************
-* void* reference_value_manager(void* ptr)
+* void* controller_arming_manager(void* ptr)
 *
 * Detects start conditions to control arming the controller.
 *******************************************************************************/
-void* reference_value_manager(void* ptr){
+void* controller_arming_manager(void* ptr){
 	// wait for IMU to settle
 	disarm_controller();
 	rc_usleep(2500000);
 		
 	while(rc_get_state()!=EXITING){
 		// sleep at beginning of loop so we can use the 'continue' statement
-		rc_usleep(1000000/REFERENCE_VALUE_MANAGER_HZ); 
+		rc_usleep(1000000/CONTROLLER_ARMING_MANAGER_HZ); 
 		
 		// nothing to do if paused, go back to beginning of loop
 		if(rc_get_state() != RUNNING) continue;
@@ -206,7 +240,7 @@ void* reference_value_manager(void* ptr){
 		// if we got here the state is RUNNING, but controller is not
 		// necessarily armed. If DISARMED, wait for the user to send start signal
 		// which will we detected by is_flying()
-		if(reference_value.armstate == DISARMED){
+		if(controller_arming.armstate == DISARMED){
 			if(is_flying()==0){
 				zero_out_controller();
 				arm_controller();
@@ -223,17 +257,12 @@ void* reference_value_manager(void* ptr){
 }
 
 /*******************************************************************************
-* int control_tilt()
+* int collect_data()
 *
-* Finds orientation from sensors and sends signal to motors to meet setpoint
+* Finds orientation from sensors.
 *******************************************************************************/
-int control_tilt(){
+int collect_data(){
 
-	//brake line motor duty cycles
-	float left_pulley_duty_cycle, right_pulley_duty_cycle;
-	//weight shift motor duty cycle
-	float ws_duty;
-	
 	// find angle from accelerometer
 	orientation.x_accel = atan2(-1*data.accel[1],sqrt(pow(data.accel[2],2) + pow(data.accel[0],2)))*RAD_TO_DEG;
 
@@ -276,44 +305,20 @@ int control_tilt(){
 	//check for various exit conditions AFTER state estimate
 	
 	if(rc_get_state() == EXITING){
+		//rc_disable_servo_power_rail()
 		rc_disable_motors();
 		return 0;
 	}
 	// if controller is still ARMED while state is PAUSED, disarm it
-	if(rc_get_state()!=RUNNING && reference_value.armstate==ARMED){
+	if(rc_get_state()!=RUNNING && controller_arming.armstate==ARMED){
 		disarm_controller();
 		return 0;
 	}
 	// exit if the controller is disarmed
-	if(reference_value.armstate==DISARMED){
+	if(controller_arming.armstate==DISARMED){
 		return 0;
 	}
 	
-	/* //example of how to implement duty cycle
-	//gain of difference equation, adjusted by battery voltage
-	double K = -7 * V_NOMINAL/sys_state.battery_voltage;
-	double u1n;
-
-	//difference function for u1[n], D1(s) output is motor duty value
-	u1n=1.997*u1_buf1-0.9965*u1_buf2+1*thetaR_buf0*K-2*0.99965*thetaR_buf1*K+0.99965*0.99965*thetaR_buf2*K;
-	
-	//normalize duty cycle - this part might be wrong, maybe if > vmax {v=vmax} and if < vmin {v=vmin}******
-	u1n = u1n/V_NOMINAL;
-	
-	//buffer u1 values for use in difference functions
-	u1_buf2 = u1_buf1;
-	u1_buf1 = u1_buf0;
-	u1_buf0 = u1n;
-
-	//set u state to u1[n]
-	sys_state.u1 = u1n;
-		
-	//Send signal to motors
-	left_duty_cycle = sys_state.u1;
-	right_duty_cycle = sys_state.u1;	
-	set_motor(MOTOR_CHANNEL_L, MOTOR_POLARITY_L * left_duty_cycle); 
-	set_motor(MOTOR_CHANNEL_R, MOTOR_POLARITY_R * right_duty_cycle); 
-	*/
 	return 0;
 }
 
@@ -323,46 +328,41 @@ int control_tilt(){
 * Sets all controller values to zero.
 *******************************************************************************/
 int zero_out_controller(){
-	//set all difference equation buffers to zero
-	/*
-	u1_buf0 =0;
-	u1_buf1=0;
-	u1_buf2=0;
-
-	thetaR_buf0=0;
-	thetaR_buf1=0;
-	thetaR_buf2=0;
-
-	u2_buf0=0;
-	u2_buf1=0;
-
-	phiR_buf0=0;
-	phiR_buf1=0;
-	*/
-	reference_value.theta = 0.0;
-	reference_value.phi   = 0.0;
 	rc_set_motor_all(0);
 	return 0;
 }
 
-//disable motors in case something happens
-int disarm_controller(){
-	rc_disable_motors();
-	reference_value.armstate = DISARMED;
-	return 0;
-}
-
-//turn the encoders and motors on and initialize their values
+/*******************************************************************************
+* int arm_controller()
+*
+* Turns the motors on and initialize their values.
+*******************************************************************************/
 int arm_controller(){
 	zero_out_controller();
-	rc_set_encoder_pos(ENCODER_CHANNEL_L,0);
-	rc_set_encoder_pos(ENCODER_CHANNEL_R,0);
-	reference_value.armstate = ARMED;
+	controller_arming.armstate = ARMED;
 	rc_enable_motors();
+
+	//can also use servo rails if needed
+	// rc_enable_servo_power_rail();
 	return 0;
 }
 
-//thread to check battery voltage
+/*******************************************************************************
+* int disarm_controller()
+*
+* Disable motors in case something happens.
+*******************************************************************************/
+int disarm_controller(){
+	rc_disable_motors();
+	controller_arming.armstate = DISARMED;
+	return 0;
+}
+
+/*******************************************************************************
+* void* battery_checker(void* ptr)
+*
+* Checks battery voltage to ensure it's normal.
+*******************************************************************************/
 void* battery_checker(void* ptr){
 		float new_v;
 		while(rc_get_state()!=EXITING){
@@ -388,9 +388,10 @@ void* printf_loop(void* ptr){
 		if(new_state==RUNNING && last_state!=RUNNING){
 			printf("\nRUNNING\n");
 			printf("  Pitch  |");
-			printf("  Pitch Reference  |");
 			printf("  Roll  |");
-			printf("  Roll Reference  |");
+			printf("  WS Duty  |");
+			printf("  BL Duty L  |");
+			printf("  BL Duty R  |");
 			printf("  Battery Voltage  |");
 			printf("  Armstate  |");
 			printf("\n");
@@ -404,14 +405,12 @@ void* printf_loop(void* ptr){
 		if(new_state == RUNNING){	
 			printf("\r");
 			printf("%7.2f  |", sys_state.angle_about_x_axis);
-			printf("%17.2f  |", reference_value.angle_about_x_axis_ref);
-
 			printf("%6.2f  |", sys_state.angle_about_y_axis);
-			printf("%16.2f  |", reference_value.angle_about_y_axis_ref);
-			
+			printf("%8.2f  |", sys_state.WS_duty_signal);
+			printf("%11.2f  |", sys_state.BL_duty_signal_left);
+			printf("%11.2f  |", sys_state.BL_duty_signal_right);
 			printf("%17.2f  |", sys_state.battery_voltage);
-			
-			printf("%10.2d  |", reference_value.armstate);
+			printf("%10.2d  |", controller_arming.armstate);
 			fflush(stdout);
 		}
 		rc_usleep(1000000 / PRINTF_HZ);
@@ -419,11 +418,34 @@ void* printf_loop(void* ptr){
 	return NULL;
 }
 
+/*******************************************************************************
+* void* read_input(void* ptr)
+*
+* Reads user input from radio transmitter. 
+*******************************************************************************/
 void* read_input(void* ptr){
 	while(rc_get_state()!=EXITING){
-		sys_state.start_cond=1; //temporary until implement user input
 
-		rc_usleep(1000000 / GET_INPUT_HZ);
+		//For the normalized data to be accurate, you must run the rc_calibrate_dsm2 example program!!!
+		//normalized reading between -1,1 depending on how far switch was moved
+		sys_state.WS_duty_signal = rc_get_dsm_ch_normalized(WS_RADIO_CHANNEL);
+		sys_state.BL_duty_signal_left = rc_get_dsm_ch_normalized(BL_RADIO_CHANNEL_L);
+		sys_state.BL_duty_signal_right = rc_get_dsm_ch_normalized(BL_RADIO_CHANNEL_R);
+		
+		//signal PWM duty to motor driver
+		rc_set_motor(WS_MOTOR_CHANNEL, sys_state.WS_duty_signal); 
+		rc_set_motor(BL_MOTOR_CHANNEL_L, BL_MOTOR_POLARITY_L * sys_state.BL_duty_signal_left); 
+		rc_set_motor(BL_MOTOR_CHANNEL_R, BL_MOTOR_POLARITY_R * sys_state.BL_duty_signal_right); 
+
+		//can use servo rails maybe
+		//need to map duty cycle to us pulse width (NOT MODULATED BY FREQ, different from PWM)
+		//rc_send_servo_pulse_us(WS_MOTOR_CHANNEL, int us)
+
+		//or gps headers as gpio
+		//create thread with freq of pwm? where toggles high/low
+		//rc_gpio_set_value(WS_MOTOR_CHANNEL, HIGH);
+
+		rc_usleep(1000000 / READ_INPUT_HZ);
 	}
 	return NULL;
 }
@@ -431,12 +453,12 @@ void* read_input(void* ptr){
 /*******************************************************************************
 * int is_flying()
 *
-* Checks if it should start controlling the motors
+* Checks if it should start controlling the motors yet.
 *******************************************************************************/
 int is_flying(){
 	const int check_hz = 20;
 	int wait_us = 1000000/check_hz;
-	if(sys_state.start_cond == 1){
+	if(rc_is_dsm_active()){
 		return 0;
 	}
 	else{
@@ -444,10 +466,11 @@ int is_flying(){
 	}
 	return -1;
 }
+
 /*******************************************************************************
 * int on_pause_pressed()
 *
-* Quits if pause button is held.
+* Quits if pause button is held. Mainly for debugging.
 *******************************************************************************/
 int on_pause_pressed(){
 	int i=0;
@@ -462,6 +485,12 @@ int on_pause_pressed(){
 	rc_set_state(EXITING);
 	return 0;
 }
+
+/*******************************************************************************
+* int on_pause_released()
+*
+* Pauses if pause button is pressed. Mainly for debugging.
+*******************************************************************************/
 int on_pause_released(){
 	// toggle betewen paused and running modes
 	if(rc_get_state()==RUNNING)   		rc_set_state(PAUSED);
