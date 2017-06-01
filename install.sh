b3725ad27674ae8cfb6261d5f6e969cc4a785415
alevesque <levesque.alejandro@gmail.com>
2017-06-01 01:14:32 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
+mv UCSD-PROTECTED.config /var/lib/connman
+connmanctl enable wifi
+connmanctl scan wifi
+connmanctl services
+connmanctl agent on
+connmanctl connect wifi_f45eab3bc364_554353442d50524f544543544544_managed_ieee8021x
 apt-get update
-apt-get -y install libconfig8-dev
-
+apt-get -y upgrade
+apt-get install libconfig8-dev
+rc_calibrate_gyro
+reboot
